78e735e465b548c2bc7403bcb667d27a5a7a1561
MixFon <mixfon@rambler.ru>
2020-12-04 15:13:11 +0300

    chenge podfile

--- Classes/ArticleManager.swift
+++ Classes/ArticleManager.swift
@@ -9,12 +9,14 @@ import Foundation
 import CoreData
 
 @available(iOS 10.0, *)
-class ArticleManager {
+public class ArticleManager {
+
+    public var managedObjectContext: NSManagedObjectContext
     
     let allLanguage = ["ENG", "RUS", "UKR", "USA"]
-    
-    func newArticle() -> Article {
-        let article = Article()
+   
+    public func newArticle() -> Article {
+        let article = Article(context: managedObjectContext)
         article.content = String.randomString(length: 20)
         article.language = allLanguage[Int(arc4random_uniform(4))]
         article.title = String.randomString(length: 5)
@@ -23,36 +25,70 @@ class ArticleManager {
         return article
     }
     
-    func getContext() -> NSManagedObjectContext{
-        let persistentContainer: NSPersistentContainer = {
-              let container = NSPersistentContainer(name: "SavingLearn")
-              container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                  if let error = error as NSError? {
-                      fatalError("Unresolved error \(error), \(error.userInfo)")
-                  }
-              })
-              return container
-          }()
+    public init() {
+        // Создаем переменную для доступа к данным пакета
+        let bundle = Bundle(for: Article.self)
         
-        let context: NSManagedObjectContext = {
-            return persistentContainer.viewContext
-          }()
-        return context
-    }
-    
-    func getAllArticles() -> [Article] {
-        let context = getContext()
-        // Запрос на получение всех данных
-        let fetchRequest: NSFetchRequest<Article> = Article.fetchRequest()
-        var objects: [Article] = []
-        do {
-            objects = try context.fetch(fetchRequest)
-        } catch let error as NSError {
-            print(error.localizedDescription)
+        // Для инициализации managedObjectContext необходимо получить:
+        // modelObject->PersistenStoreCoordinator->URL_на_базу данных->Связать
+        
+        // Инициализируем обьект managedObjectContext
+        guard let modelURL = bundle.url(forResource: "article", withExtension: "momd") else {
+            fatalError("Error bondle.url")
         }
-        return objects
+        guard let managerObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Initialization NSManagedObjectModel")
+        }
+        
+        // Создаем PersistenStoreCoordinator на основе только что созданной модели.
+        let presistenStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managerObjectModel)
+        
+        // Создаем ссылку на БД
+        let storeUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("article.sqlite")
+        
+        // Далее необходимо связвть PersistenStoreCoordinator c PersistentStore
+        do{
+            try presistenStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeUrl, options: nil)
+        } catch {
+            fatalError("Error addPersistentStore")
+        }
+        // Инициализируем контекст и связваем его с созданным координатором
+        // Запускаем в оидельной очереди
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = presistenStoreCoordinator
     }
     
+        public func getAllArticles() -> [Article] {
+            //let context = getContext()
+            // Запрос на получение всех данных
+            let fetchRequest: NSFetchRequest<Article> = Article.fetchRequest()
+            var objects: [Article] = []
+            do {
+                objects = try managedObjectContext.fetch(fetchRequest)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            return objects
+        }
+    
+//    public func getContext() -> NSManagedObjectContext{
+//        let persistentContainer: NSPersistentContainer = {
+//              let container = NSPersistentContainer(name: "article")
+//              container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//                  if let error = error as NSError? {
+//                      fatalError("Unresolved error \(error), \(error.userInfo)")
+//                  }
+//              })
+//              return container
+//          }()
+//
+//        let context: NSManagedObjectContext = {
+//            return persistentContainer.viewContext
+//          }()
+//        return context
+//    }
+
+
 }
 
 extension String {
@@ -61,3 +97,19 @@ extension String {
       return String((0..<length).map{ _ in letters.randomElement()! })
     }
 }
+
+//public override init(){
+//    let persistentContainer: NSPersistentContainer = {
+//          let container = NSPersistentContainer(name: "Article")
+//          container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//              if let error = error as NSError? {
+//                  fatalError("Unresolved error \(error), \(error.userInfo)")
+//              }
+//          })
+//          return container
+//      }()
+//    managedObjectContext = persistentContainer.viewContext
+//    print("Hello")
+//    print(managedObjectContext)
+//
+//}
--- Example/widraugr2020/ViewController.swift
+++ Example/widraugr2020/ViewController.swift
@@ -14,10 +14,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let temp = Article()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let articleManager = ArticleManager()
+        let articles = articleManager.getAllArticles()
+        if (articles.isEmpty) {
+            print("article is empty")
+        }else {
+            for article in articles {
+                print(article)
+            }
+        }
+        //let newArticle = articleManager.newArticle()
+        //print(newArticle)
+        do {
+            try articleManager.managedObjectContext.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
